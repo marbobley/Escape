b29cadfa9a1f5e100810a39481c8114b0439ea83
Marbobley <fournier.wilf@gmail.com>
2025-10-11 16:46:29 +0200

    playing with js

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,6 @@ import './bootstrap.js';
 import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './scripts/spinner-stop.js'
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ assets/scripts/spinner-stop.js
@@ -0,0 +1,1 @@
+document.getElementById('spinner').style.animation = 'none';

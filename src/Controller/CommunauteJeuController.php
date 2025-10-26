6bc33fa5d6985d52d48c16951ad55061e3966437
Marbobley <fournier.wilf@gmail.com>
2025-10-26 13:47:29 +0100

    ongoing

--- src/Controller/CommunauteJeuController.php
+++ src/Controller/CommunauteJeuController.php
@@ -5,22 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/communaute')]
 final class CommunauteJeuController extends AbstractController
 {
-    #[Route('/communaute', name: 'app_communaute_jeu_index', methods: ['GET'])]
+    #[Route('', name: 'app_communaute_jeu_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('communaute-jeu/index.html.twig', [
-            'controller_name' => 'SSGController',
-        ]);
+        return $this->render('communaute-jeu/index.html.twig', []);
     }
 
-    #[Route('/communaute/premier_niveau', name: 'app_communaute_jeu_premier_niveau', methods: ['GET'])]
+    #[Route('/premier_niveau', name: 'app_communaute_jeu_premier_niveau', methods: ['GET'])]
     public function premier_niveau(): Response
     {
-        return $this->render('communaute-jeu/premier_niveau.html.twig', [
-            'controller_name' => 'SSGController',
-        ]);
+        return $this->render('communaute-jeu/premier_niveau.html.twig', []);
     }
+
+    #[Route('/premier_niveau/premier_pierre_descendre', name: 'app_communaute_jeu_premier_pierre_descendre', methods: ['GET'])]
+    public function premiere_pierre() : Response
+    {
+        return $this->render('communaute-jeu/premier_niveau_premier_pierre_descendre.html.twig', []);
+    }
+
+    #[Route('/premier_niveau/premier_pierre_enlever', name: 'app_communaute_jeu_premier_pierre_enlever', methods: ['GET'])]
+    public function premiere_pierre_enelever() : Response
+    {
+        return $this->render('communaute-jeu/premier_niveau_premier_pierre_enlever.html.twig', []);
+    }
+
 }

a0aff256159c49228890f1d3b6330d350f3519ab
Marbobley <fournier.wilf@gmail.com>
2025-10-26 19:00:02 +0100

    feat : add timestamping

--- src/Controller/CommunauteJeuController.php
+++ src/Controller/CommunauteJeuController.php
@@ -22,6 +22,12 @@ final class CommunauteJeuController extends AbstractController
     #[Route('/premier_niveau', name: 'app_communaute_jeu_premier_niveau', methods: ['GET'])]
     public function premier_niveau(Request $request): Response
     {
+        $session = $request->getSession();
+        $startDay = $session->get('start');
+        if(!isset($startDay))
+        {
+            $session->set('start', new \DateTimeImmutable('now'));
+        }
         return $this->render('communaute-jeu/premier_niveau.html.twig', []);
     }
 
@@ -60,6 +66,7 @@ final class CommunauteJeuController extends AbstractController
         $session->set('clef-4', true);
         return $this->render('communaute-jeu/premier_niveau_deuxieme_pierre_enlever.html.twig', []);
     }
+
 
     #[Route('/premier_niveau/deuxieme_niveau', name: 'app_communaute_jeu_deux_ouvert', methods: ['GET','POST'])]
     public function jeu_deux(Request $request): Response{

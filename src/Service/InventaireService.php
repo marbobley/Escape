599d7a43381af22556f8d10e6814aabf30102be0
Marbobley <fournier.wilf@gmail.com>
2025-11-01 12:46:23 +0100

    feat : add service inventaire

--- src/Service/InventaireService.php
+++ src/Service/InventaireService.php
@@ -4,10 +4,19 @@ namespace App\Service;
 
 use App\Model\Inventaire;
 use App\Model\ObjetAventure;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class InventaireService
 {
+    public function getInventaireObject(SessionInterface $session , string $key) : ObjetAventure|null {
+        $inventaire = $session->get('inventaire');
 
+        if($inventaire instanceof Inventaire && array_key_exists($key, $inventaire->getContenu())){
+            return $inventaire->getContenu()[$key];
+        }
+
+        return null;
+    }
     public function addOrReplace(string $key, ObjetAventure $obj , Inventaire $currentInventaire)
     {
        $content = $currentInventaire->getContenu();
